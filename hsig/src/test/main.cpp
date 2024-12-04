a288fa7ff5afe97e5eefaf30dc12293f117a623c
sus0pid <s2136500@ed.ac.uk>
2024-12-04 17:17:48 +0000

    test helloworld hsig

--- hsig/src/test/main.cpp
+++ hsig/src/test/main.cpp
@@ -1,20 +1,42 @@
 #include <iostream>
 
-#include "../hsig.hpp"
+#include "hsig.hpp"
+
 
 int main() {
-  hybrid_sig::HybridSigConfig config{256, 5, 10, std::chrono::milliseconds(1000)};
-  hybrid_sig::HybridSignature hs(config);
+  hybrid_sig::HybridSignature hsig;
 
-  hs.start_background();
+  std::string data = "example_data";
+  std::string signature = hsig.sign(data);
 
-  const uint8_t message[] = "Hello, World!";
-  auto signature = hs.sign(message, sizeof(message));
-  bool valid = hs.verify(message, sizeof(message), signature);
-
-  std::cout << "Signature valid: " << std::boolalpha << valid << "\n";
-
-  hs.stop_background();
+  if (hsig.verify(data, signature)) {
+    std::cout << "Verification succeeded!" << std::endl;
+  } else {
+    std::cout << "Verification failed!" << std::endl;
+  }
 
   return 0;
 }
+
+
+
+//#include <iostream>
+//
+//#include "../hsig.hpp"
+//
+//int main() {
+//  hybrid_sig::HybridSigConfig config{256, 5, 10, std::chrono::milliseconds(1000)};
+//  hybrid_sig::HybridSignature hs(config);
+//
+//  hs.start_background();
+//
+//  const uint8_t message[] = "Hello, World!";
+//  auto signature = hs.sign(message, sizeof(message));
+//  bool valid = hs.verify(message, sizeof(message), signature);
+//
+//  std::cout << "Signature valid: " << std::boolalpha << valid << "\n";
+//
+//  hs.stop_background();
+//
+//  return 0;
+//}
